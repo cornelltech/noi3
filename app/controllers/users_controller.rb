466afc7270ad016e36562bece85490064fdc9e18
Denny Tek <tek.denny@gmail.com>
2016-09-22 12:27:21 -0400

    Add user profile rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
 
   def index
-    @user = "DENNNY"
+    @users = User.all
     @categories = ["open data", "crowdsourcing", "data science", "community engagement", "lab design", "prized-challenged", "design thinking", "citizen science"]
   end
+
+  def fetch_user 
+    @selected = User.find(params[:user_id])
+    respond_to do | format |
+      format.js
+    end
+  end
+  
 
 end
