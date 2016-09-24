540743dbd156b8f8f3d2b2106345d8af3a2587e4
Denny Tek <tek.denny@gmail.com>
2016-09-24 16:18:04 -0400

    Change user path name to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-    # resources :users
+  resources :users, :path => 'search'
   root 'pages#index'
-  get 'search', :to => 'users#index'
-  get '/fetch_user' => "users#fetch_user", as: "fetch_user"
 end
