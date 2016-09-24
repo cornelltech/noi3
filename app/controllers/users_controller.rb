cdb4a9a933fb134e0f93c60d26ceaef7ec76c2e4
Denny Tek <tek.denny@gmail.com>
2016-09-24 16:17:38 -0400

    Add more data to json response for /search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
+require 'byebug'
 class UsersController < ApplicationController
 
   def index
     @users = User.all
     @categories = ["open data", "crowdsourcing", "data science", "community engagement", "lab design", "prized-challenged", "design thinking", "citizen science"]
+  end
+
+  def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html 
+      format.json { render json: {:user => @user, :projects => @user.projects, :events => @user.events, :categories => @user.categories}}
+    end
   end
 
   def fetch_user 
@@ -12,5 +21,10 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :email, :password, :points)
+  end
 
 end
