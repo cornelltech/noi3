1f6263be7c77a3cec26fc562c0ec75520caaf7c6
Denny Tek <tek.denny@gmail.com>
2016-09-26 16:24:59 -0400

    Add devise helpers for form_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+
+  # DEVISE HELPERS for forms
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+    
 end
