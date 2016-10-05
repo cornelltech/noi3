4f3e6686b2f640e590cbdfc86636f474033f9d82
naffis <davidnaffis@gmail.com>
2016-10-04 23:24:54 -0400

    revert discourse config and user creation for others in dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   attr_accessor :can_teach
   attr_accessor :can_learn
   
-  after_create :create_discourse_user
+  # after_create :create_discourse_user
   # after_update :update_discourse_sso
 
   def country
