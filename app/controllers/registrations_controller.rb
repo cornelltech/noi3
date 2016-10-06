0c162012e380715d63e4a15841848ab6d835692b
mocxd <mark.adkins.hastings@gmail.com>
2016-10-06 01:14:34 -0400

    basic success state for signup, needs styles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,11 +10,18 @@ class RegistrationsController < Devise::RegistrationsController
             if resource.active_for_authentication?
                 set_flash_message! :notice, :signed_up
                 sign_up(resource_name, resource)
-                respond_with resource, location: after_sign_up_path_for(resource)
+                # respond_with resource, location: '/search' and return
+                respond_to do |format|
+                    format.js {
+                        render :file => "/pages/fetch_sign_up_success.js.erb"
+                    }
+                end
             else
+                set_flash_message! :notice, :signed_up
                 set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
                 expire_data_after_sign_in!
-                respond_with resource, location: after_inactive_sign_up_path_for(resource)
+                # respond_with resource, location: '/search' and return
+                # redirect_to action: "index", controller: "users"
             end
         else
             clean_up_passwords resource
@@ -24,7 +31,6 @@ class RegistrationsController < Devise::RegistrationsController
                 format.js {
                     render :file => "/pages/fetch_sign_up.js.erb"
                 }
-
             end
         end
     end
