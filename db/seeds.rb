af123345c91b48d48b7b2ac1074e682dc6005852
naffis <davidnaffis@gmail.com>
2016-10-06 17:21:21 -0400

    changes for import/migration from noi2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,22 +16,22 @@ end
 Category.all.each { | category | Survey.create(category_id: category.id)}
 
 
-# FAKE DATA for testing
-20.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: 'password',
-    username: Faker::Internet.user_name,
-    picture_path: '/assets/users/128.jpg',
-    position: Faker::Company.profession,
-    organization: Faker::Company.name,
-    organization_type: Faker::Commerce.department,
-    city: Faker::Address.city,
-    country_code: Faker::Address.country_code
-    )
-end
+# # FAKE DATA for testing
+# 20.times do
+#   User.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: 'password',
+#     username: Faker::Internet.user_name,
+#     picture_path: '/assets/users/128.jpg',
+#     position: Faker::Company.profession,
+#     organization: Faker::Company.name,
+#     organization_type: Faker::Commerce.department,
+#     city: Faker::Address.city,
+#     country_code: Faker::Address.country_code
+#     )
+# end
 
 # GENERATE CATEGORIES/SKILL AREAS/SKILLS via CSV Questionnaire
 csv_file_path = 'db/test-questionnaire-noi.csv'
@@ -52,58 +52,59 @@ end
 
 # OTHER SEED DATA
 
-50.times do
-  Project.create!(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    url: Faker::Internet.url('example.com'),
-    user_id: rand(1..20)
-    )
-end
+# 50.times do
+#   Project.create!(
+#     title: Faker::Commerce.product_name,
+#     description: Faker::Lorem.paragraph,
+#     url: Faker::Internet.url('example.com'),
+#     user_id: rand(1..20)
+#     )
+# end
 
-3.times do
-  User.all.each {|user| user.categories << Category.find(rand(1..8)) }
-end
+# 3.times do
+#   User.all.each {|user| user.categories << Category.find(rand(1..8)) }
+# end
 
 Event.create(conference_code: "IODC", name: "IODC 2016", logo_path: "logo-iodc-web.png")
 Event.create(conference_code: "ODRS", name: "Open Data Research Symposium ", logo_path: "2016-odrs.png")
 
-User.all.each { | user | user.events<< Event.first }
+# User.all.each { | user | user.events<< Event.first }
 
 # Dummy Industries
-industries =
-['Accounting','Agriculture','Animal Rights','Architecture & Planning','Arts & Culture','Aviation & Aerospace','Biotechnology','Business Licensing & Regulation','Chemicals','Children\'s Rights','Civic & Social Organization','Civil Rights & Civil Liberties','Computer Software','Construction & Civil Engineering','Consumer Protection/Debt','Consumer Services','Cyber Security','Design','Disability Rights','Economic Development','Education','Electricity','Employment & Labor','Energy','Environmental Services','Events Services','Executive Office','Facilities Services','Financial Services','Food and Drug','Foreign Affairs','Fund-Raising','Government Efficiency, Transparency & Accountability','Government Relations','Graphic Design','Health Care','Higher Education','Hospitality','Housing, Real Estate & Land Use','Human Resources','Human Rights','Immigration & Citizenship Services','Import & Export','Industrial Automation','Information Services & Technology','Insurance','Intellectual Property and Cyber Rights','International Affairs','International Aid & Development','International Trade','Internet','Judiciary','Juvenile Issues','Law Enforcement','Law & Legal Services','Legislative Policy','Logistics and Supply Chain','Management Consulting','Maritime','Market Research','Media & Communications','Medical Practice','Municipal','Museums and Institutions','National Security & Military','Nonprofit Organization Management','Philanthropy','Political Campaigns/Election','Political Organization','Public Benefits/Social Security','Public Policy','Public Relations','Public Safety & Emergencies','Racial Justice','Regulatory Affairs','Reproductive Rights','Research','Sanitation','Science & Innovation','Social Services','Space','Sports','Tax & Revenue','Telecommunications','Think Tanks','Trade & Investment','Transportation / Trucking / Railroad','Utilities & Water Resources','Women\'s Rights','Other']
+# industries =
+# ['Accounting','Agriculture','Animal Rights','Architecture & Planning','Arts & Culture','Aviation & Aerospace','Biotechnology','Business Licensing & Regulation','Chemicals','Children\'s Rights','Civic & Social Organization','Civil Rights & Civil Liberties','Computer Software','Construction & Civil Engineering','Consumer Protection/Debt','Consumer Services','Cyber Security','Design','Disability Rights','Economic Development','Education','Electricity','Employment & Labor','Energy','Environmental Services','Events Services','Executive Office','Facilities Services','Financial Services','Food and Drug','Foreign Affairs','Fund-Raising','Government Efficiency, Transparency & Accountability','Government Relations','Graphic Design','Health Care','Higher Education','Hospitality','Housing, Real Estate & Land Use','Human Resources','Human Rights','Immigration & Citizenship Services','Import & Export','Industrial Automation','Information Services & Technology','Insurance','Intellectual Property and Cyber Rights','International Affairs','International Aid & Development','International Trade','Internet','Judiciary','Juvenile Issues','Law Enforcement','Law & Legal Services','Legislative Policy','Logistics and Supply Chain','Management Consulting','Maritime','Market Research','Media & Communications','Medical Practice','Municipal','Museums and Institutions','National Security & Military','Nonprofit Organization Management','Philanthropy','Political Campaigns/Election','Political Organization','Public Benefits/Social Security','Public Policy','Public Relations','Public Safety & Emergencies','Racial Justice','Regulatory Affairs','Reproductive Rights','Research','Sanitation','Science & Innovation','Social Services','Space','Sports','Tax & Revenue','Telecommunications','Think Tanks','Trade & Investment','Transportation / Trucking / Railroad','Utilities & Water Resources','Women\'s Rights','Other']
 
-industries.each { | industry |  Industry.create(name:industry) }
+# industries.each { | industry |  Industry.create(name:industry) }
 
-3.times do
-  User.all.each {|user| user.industries << Industry.all.sample }
-end
+# 3.times do
+#   User.all.each {|user| user.industries << Industry.all.sample }
+# end
 
-3.times do
-  Project.all.each {|project| project.industries << Industry.all.sample }
-end
+# 3.times do
+#   Project.all.each {|project| project.industries << Industry.all.sample }
+# end
 
-5.times do
-  Project.all.each { |project| project.skill_areas << SkillArea.find(rand(1..SkillArea.all.count)) }
-end
+# 5.times do
+#   Project.all.each { |project| project.skill_areas << SkillArea.find(rand(1..SkillArea.all.count)) }
+# end
 
 
-# # Create Teachable and Learnable relationships
-User.all.each do |user|
-  20.times do
-    Learnable.where(user_id: user.id, skill_id: rand(1..Skill.all.count)).first_or_create
-    Teachable.where(user_id: user.id, skill_id: rand(1..Skill.all.count)).first_or_create
-  end
-end
+# # # Create Teachable and Learnable relationships
+# User.all.each do |user|
+#   20.times do
+#     Learnable.where(user_id: user.id, skill_id: rand(1..Skill.all.count)).first_or_create
+#     Teachable.where(user_id: user.id, skill_id: rand(1..Skill.all.count)).first_or_create
+#   end
+# end
 
 # Teachable.create(user_id: 1, skill_id:1)
 # Teachable.create(user_id: 1, skill_id:2)
 
-User.create!(email: 'admin@example.com', password: 'password', username: 'adminuname', password_confirmation: 'password', :admin => true)
+u = User.new(email: 'admin@example.com', password: 'password', username: 'adminuname', password_confirmation: 'password', :admin => true)
+u.skip_confirmation!
+u.save!
 
-
-User.create(email: 'test@example.com', password: 'password', username: 'testusername',
+u = User.new(email: 'test@example.com', password: 'password', username: 'testusername',
   first_name: "TEST",
   last_name: "USER",
   picture_path: '/assets/users/128.jpg',
@@ -112,3 +113,5 @@ User.create(email: 'test@example.com', password: 'password', username: 'testuser
   organization_type: Faker::Commerce.department,
   city: Faker::Address.city,
   country_code: Faker::Address.country_code)
+u.skip_confirmation!
+u.save!
--- lib/tasks/import.rb
+++ lib/tasks/import.rb
@@ -12,76 +12,79 @@ class OldDatabase < ActiveRecord::Base
 end
 
 class OldUser < OldDatabase
-self.table_name = "users"
+	self.table_name = "users"
 end
 
 class OldConference < OldDatabase
-self.table_name = "user_conferences"
+	self.table_name = "user_conferences"
 end
 
 class OldUserExpertiseDomains < OldDatabase
-self.table_name = "user_expertise_domains"
+	self.table_name = "user_expertise_domains"
 end
 
 class OldUserLanguages < OldDatabase
-self.table_name = "user_languages"
+	self.table_name = "user_languages"
 end
 
 class OldUserSkills < OldDatabase
-self.table_name = "user_skills"
+	self.table_name = "user_skills"
 end
 
-for user in OldUser.all
+# for user in OldUser.all
+# 	# create or update user	
+# 	puts "creating user for #{user.username}"
+# 	u = User.where(:username => user.username).first
+# 	u = User.new() unless u
+# 	u.attributes = {
+# 		:first_name => user.first_name,
+# 		:last_name => user.last_name,
+# 		:email => user.email,
+# 		:position => user.position,
+# 		:organization => user.organization,
+# 		:organization_type => user.organization_type,
+# 		:city => user.city,
+# 		:username => user.username,
+# 		:country_code => user.country,
+# 		:picture_path => "/networkofinnovators.org/static/pictures/#{user.id}/#{user.picture_id}",
+# 		:password => 'gasdgagasg235252352asdgsa',
+# 		:password_confirmation => 'gasdgagasg235252352asdgsa' 
+# 	}
+# 	u.skip_confirmation!
+# 	u.save!
 
-# create or update user	
-u = User.where(:username => user.username).first
-u = User.new() unless u
-u.attributes = {
-	:first_name => user.first_name,
-	:last_name => user.last_name,
-	:email => user.email,
-	:position => user.position,
-	:organization => user.organization,
-	:organization_type => user.organization_type,
-	:city => user.city,
-	:username => user.username,
-	:country_code => user.country,
-	:picture_path => "/networkofinnovators.org/static/pictures/#{user.id}/#{user.picture_id}",
-	:password => 'gasdgagasg235252352asdgsa',
-	:password_confirmation => 'gasdgagasg235252352asdgsa' 
-}
-u.skip_confirmation!
-u.save!
+# 	# add events to user
+# 	conferences = OldConference.where(:user_id => user.id)	
+# 	for conference in conferences
+# 		puts "Adding event #{conference.conference} for #{user.username}"
+# 		if conference.conference == "iodc16"
+# 			e = Event.where(:conference_code => 'IODC')
+# 			u.events << e
+# 		elsif conference.conference == "odrs16"
+# 			e = Event.where(:conference_code => 'ODRS')
+# 			u.events << e
+# 		end
+# 	end
 
-# add events to user
-conferences = OldConference.where(:user_id => user.id)
-for conference in conferences
-	if conference.conference == "iodc16"
-		e = Event.where(:conference_code => 'IODC')
-		u.events << e
-	elsif conference.conference == "odrs16"
-		e = Event.where(:conference_code => 'ODRS')
-		u.events << e
-	end
-end
+# 	# add industries to user
+# 	industries = OldUserExpertiseDomains.where(:user_id => user.id)
+# 	for industry in industries
+# 		name = industry.name.split(",").first
+# 		i = Industry.where(:name => name).first
+# 		i = Industry.new(:name => name) unless i
+# 		puts "Adding industry #{i.name} for #{user.username}"
+# 		u.industries << i
+# 	end
 
-# add industries to user
-industries = OldUserExpertiseDomains.where(:user_id => user.id)
-for industry in industries
-	name = industry.name.split(",").first
-	i = Industry.where(:name => name).first
-	i = Industry.new unless i
-	u.industries << i
-end
+# 	# add user languages
+# 	languages = OldUserLanguages.where(:user_id => user.id)
+# 	for language in languages
+# 		lang = Language.where(:abbreviation => language.locale)
+# 		puts "Adding language #{language.locale} for #{user.username}"
+# 		u.languages << lang
+# 	end
 
-# add user languages
-languages = OldUserLanguages.where(:user_id => user.id)
-for language in languages
-	language = Language.where(:abbreviation => language.locale)
-	u.languages << language
-end
-
-end
+# end
 
 # add user skills (god help us)
 y = YAML.load_file('db/questions.yaml')
@@ -94,19 +97,34 @@ for category in y
 		for question in questions
 			label = question['label']
 			question_name = "#{name.parameterize}-#{topic_name.parameterize}-#{label.parameterize}"
-
-			user_skills = OldUserSkills.where(:name => question_name)
+			puts question_name
+			user_skills = OldUserSkills.where(:name => question_name)			
 			for user_skill in user_skills
+				puts user_skill
 				old_user = OldUser.find(user_skill.user_id)
 				new_user = User.where(:username => old_user.username).first
 				if new_user
-					new_skill = Skill.where(:short_name => "#{name.parameterize}-#{topic_name.parameterize}",
-					:description => question['question'])
-					
-					if user_skill.level < 0
-						new_user.learnables << new_skill	
-					else
-						new_user.teachables << new_skill
+					s_name = name.parameterize
+					s_name = "open-data" if s_name == "opendata"
+					s_name = "citizen-science" if s_name == "citizenscience"
+					s_name = "data-science"		if s_name == "datascience"
+					s_name = "community-engagement"	if s_name == "communityengagement"					
+					puts "looking for short_name starting with: #{s_name}"
+					puts "question is #{question['question']}"					
+					new_skill = Skill.where("short_name LIKE ? AND description = ?",
+						"#{s_name}%", question['question']).first
+						# :short_name => "#{name.parameterize}-#{topic_name.parameterize}", :description => question['question'])
+					puts "found #{new_skill.inspect}"
+					if new_skill 
+						if user_skill.level < 0							
+							Learnable.create!(:user_id => new_user.id, :skill_id => new_skill.id)
+							puts "added learnable #{new_skill.short_name} to #{new_user.username}"
+							# new_user.learnables << new_skill	
+						else
+							Teachable.create!(:user_id => new_user.id, :skill_id => new_skill.id)
+							puts "added teachable #{new_skill.short_name} to #{new_user.username}"
+							# new_user.teachables << new_skill
+						end
 					end
 				end
 			end
