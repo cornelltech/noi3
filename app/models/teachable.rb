5340c84481da5a77cb38d148b1d80eecd82bf36a
Denny Tek <tek.denny@gmail.com>
2016-10-11 18:05:20 -0400

    Add eager loading to match me calls

--- app/models/teachable.rb
+++ app/models/teachable.rb
@@ -2,7 +2,6 @@ class Teachable < ApplicationRecord
   belongs_to :user
   belongs_to :skill
   has_one :category, :through => :skill
-  has_one :skill_area, :through => :skill
 
   def panel_formatted
     self.skill.description 
