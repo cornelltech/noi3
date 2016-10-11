be2177cadec98edbc6ca2663a0ecbebbb00e92ab
Denny Tek <tek.denny@gmail.com>
2016-10-11 17:31:09 -0400

    Add eager loading to db calls"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -93,14 +93,14 @@ class UsersController < ApplicationController
   end
 
   def fetch_user_teaching_menu
-    @surveys = Survey.all
+    @surveys = Survey.includes(:category).all
   end
 
   def fetch_learning_survey
     @user = current_user
     @survey = Survey.find(params[:survey_id])
     @user_skills_in_cat = @user.learnables.includes(:category).select {|item| item.skill.category == @survey.category }.pluck(:skill_id)
-    @skill_areas = @survey.category.skill_areas.map {|sa| sa}.uniq!
+    @skill_areas = @survey.category.skill_areas.includes(:skills).map {|sa| sa}.uniq!
     respond_to do |format|
       format.js
     end
@@ -108,9 +108,9 @@ class UsersController < ApplicationController
 
   def fetch_teaching_survey
     @user = current_user
-    @survey = Survey.find(params[:survey_id])
-    @user_skills_in_cat = @user.teachables.includes(:category).select {|item| item.skill.category == @survey.category }.pluck(:skill_id)
-    @skill_areas = @survey.category.skill_areas.map {|sa| sa}.uniq!
+    @survey = Survey.includes(:category).find(params[:survey_id])
+    @user_skills_in_cat = @user.teachables.includes(:skill, :category).select {|item| item.skill.category == @survey.category }.pluck(:skill_id)
+    @skill_areas = @survey.category.skill_areas.includes(:skills).map {|sa| sa }.uniq!
     respond_to do |format|
       format.js
     end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,8 @@
 class Survey < ApplicationRecord
   belongs_to :category
-  has_many :skill_areas, through: :categories
-  has_many :skills, through: :categories
-  accepts_nested_attributes_for :skills
+  has_many :skill_areas, through: :category
+  has_many :skills, through: :category
+
 
 
 end
