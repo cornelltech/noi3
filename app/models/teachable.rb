bccbc1a32e49b1b196cce820311ed6aab279f0e8
Denny Tek <tek.denny@gmail.com>
2016-10-12 11:25:15 -0400

    Add skill area to teachable through skill

--- app/models/teachable.rb
+++ app/models/teachable.rb
@@ -2,6 +2,7 @@ class Teachable < ApplicationRecord
   belongs_to :user
   belongs_to :skill
   has_one :category, :through => :skill
+  has_one :skill_area, :through => :skill
 
   def panel_formatted
     self.skill.description 
