4bc3799dccf10582a9e25c27ccb7af3a37675e32
mocxd <mark.adkins.hastings@gmail.com>
2016-10-20 12:13:02 -0400

    add js response for sessions ctrl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,12 @@ class SessionsController < Devise::SessionsController
             yield resource if block_given?
             respond_with resource, location: after_sign_in_path_for(resource)
         else # error state
-            puts 'error state'
+            respond_to do |format|
+                format.js {
+                    render :file => "/pages/fetch_sign_up.js.erb"
+                }
+                format.html { respond_with resource }
+            end
         end
     end
 end
