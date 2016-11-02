17bd85b859885d1185bb209b8a51fea57cf6e957
mocxd <mark.adkins.hastings@gmail.com>
2016-11-02 12:24:47 -0400

    basic js-based response behavior for login error/success working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,18 @@ class SessionsController < Devise::SessionsController
             set_flash_message!(:notice, :signed_in)
             sign_in(resource_name, resource)
             yield resource if block_given?
-            respond_with resource, location: after_sign_in_path_for(resource)
+            # respond_with resource, location: after_sign_in_path_for(resource)
+            # byebug
+            respond_to do |format|
+                format.js {
+                    render :file => "/pages/fetch_log_in_success.js.erb"
+                }
+                format.html { respond_with resource, location: "/"  }
+            end
         else # error state
-            set_flash_message!(:notice, :error)
+            set_flash_message(:notice, :error)
+            self.resource = User.new
+            # self.resource = warden.authenticate(auth_options)
             respond_to do |format|
                 format.js {
                     render :file => "/pages/fetch_log_in.js.erb"
