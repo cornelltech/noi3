b9a59344bbf21ff024a6683ff87a102706e03d38
Ena Bek <enabek@gmail.com>
2016-11-08 11:24:20 -0500

    Add call to get api_username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,9 +40,10 @@ class ApplicationController < ActionController::Base
 
     discourse_client = DiscourseApi::Client.new(DISCOURSE_CONFIG[:url])
     discourse_client.api_key = DISCOURSE_CONFIG[:api_key]
+    discourse_client.api_username = DISCOURSE_CONFIG[:api_username]
 
     if @user && @user.username
-      notifications_json = HTTParty.get(discourse_client.host + "/notifications.json?username=" + @user.username + "&api_key=" + discourse_client.api_key + "&api_username=" + @user.username)
+      notifications_json = HTTParty.get(discourse_client.host + "/notifications.json?username=" + discourse_client.api_username + "&api_key=" + discourse_client.api_key + "&api_username=" + discourse_client.api_username)
 
       if notifications_json["notifications"]
         notifications_json["notifications"].each do |notification|
