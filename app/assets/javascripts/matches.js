ef41e4443cf58f1d57467132a301b78e25512a50
enabek <enabek@gmail.com>
2017-02-10 13:12:16 -0500

    Refactor

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -14,27 +14,25 @@ $(document).ready(function() {
   // Advise
 
   var adviseCategories = ["#advise-open-data", "#advise-data-science", "#advise-prizes"]
-  var adviseLessCategories = ["#advise-less-open-data", "#advise-less-data-science", "#advise-less-prizes"]
 
   adviseCategories.forEach(function(category) {
     $(category).click(function() {
       $(this).data('clicked', true);
+      var show = false;
 
       if($(this).data('clicked')) {
-        console.log(category, 'clicked')
         $(this).find("#advise-view-more").toggleClass("advise-view-less");
         $(this).find(".advise-all-skills").toggleClass("advise-view-less");
-      };
-    })
-  })
 
-  adviseLessCategories.forEach(function(elm) {
-    $(elm).click(function() {
-      $(this).data('clicked', true);
+        var allSkills = $(this).find("#advise-all-skills");
 
-      if($(this).data('clicked')) {
-        $(this).find("#advise-all-skills").toggleClass("advise-view-less");
-        $(this).find("#advise-view-more").toggleClass("advise-view-less");
+        if (allSkills.data('clicked')) {
+          var show = true
+          $(this).find("#advise-view-more").toggleClass("advise-view-less");
+          if(show) {
+            $(this).find("#advise-view-more").toggleClass("advise-view-less")
+          }
+        }
       };
     })
   })
