199748bcbb2b276f432d5f76d1dfd560df6b204c
enabek <enabek@gmail.com>
2017-02-10 12:15:37 -0500

    Correctly targeting category skills

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -12,15 +12,49 @@ $(document).ready(function() {
 
 
   // Advise
-  
-  $("#advise-view-more").click(function() {
-    $(this).hide();
-    $("#advise-all-skills").toggleClass("advise-view-less");
+
+  var adviseCategories = ["#advise-open-data", "#advise-data-science", "#advise-prizes"]
+  var adviseLessCategories = ["#advise-less-open-data", "#advise-less-data-science", "#advise-less-prizes"]
+
+  adviseCategories.forEach(function(category) {
+    $(category).click(function() {
+      $(this).data('clicked', true);
+
+      if($(this).data('clicked')) {
+        console.log(category, 'clicked')
+        $(this).find("#advise-view-more").hide();
+        $(this).find("#advise-all-skills").toggleClass("advise-view-less");
+      };
+    })
   })
 
-  $("#advise-view-less").click(function() {
-    $("#advise-all-skills").toggleClass("advise-view-less");
-    $("#advise-view-more").show();
-  })
+  // adviseLessCategories.forEach(function(category) {
+  //   $(category).click(function() {
+  //     $(this).data('clicked', true);
+  //     console.log(category, 'clicked')
+  //
+  //     // if($(this).data('clicked')) {
+  //     //   $(this).next("#advise-all-skills").toggleClass("advise-view-less");
+  //     //   $(this).next("#advise-view-more").show();
+  //     // };
+  //   })
+  // })
+
+  // $(category).next("#advise-view-less").click(function() {
+  //   console.log(category, 'advise clicked')
+  //   // $(this).next("#advise-all-skills").toggleClass("advise-view-less");
+  //   // $(this).next("#advise-view-more").show();
+  // })
+
+  // $("#advise-view-more").click(function() {
+  //   console.log("clicked")
+  //   $(this).hide();
+  //   $("#advise-all-skills").toggleClass("advise-view-less");
+  // })
+
+  // $("#advise-view-less").click(function() {
+  //   $("#advise-all-skills").toggleClass("advise-view-less");
+  //   $("#advise-view-more").show();
+  // })
 
 });
