01946ec920df8ebcd50a6d804ee187d05ec3a8e0
enabek <enabek@gmail.com>
2017-02-13 13:57:47 -0500

    Change IDs to classes

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
 
-  var categories = ["#open-data", "#data-science", "#prizes", "#citizen-science", "#community-engagement", "#lab-design", "#crowdsourcing", "#rcts", "#advise-rcts", "#advise-crowdsourcing", "#advise-lab-design", "#advise-open-data", "#advise-data-science", "#advise-prizes", "#advise-citizen-science", "#advise-community-engagement"]
+  var categories = [".open-data", ".data-science", ".prizes", ".citizen-science", ".community-engagement", ".lab-design", ".crowdsourcing", ".rcts", ".advise-rcts", ".advise-crowdsourcing", ".advise-lab-design", ".advise-open-data", ".advise-data-science", ".advise-prizes", ".advise-citizen-science", ".advise-community-engagement"]
 
   categories.forEach(function(category) {
     $(category).click(function() {
-      $(this).data('clicked', true);
-
-      if($(this).data('clicked')) {
-        var show = true
-
+      var show = true;
+      $(this).find("#view-more").toggleClass("view-less");
+      $(this).find(".all-skills").toggleClass("view-less");
+      
+      if(show) {
         $(this).find("#view-more").toggleClass("view-less");
-        $(this).find(".all-skills").toggleClass("view-less");
-
-        if(show) {
-          $(this).find("#view-more").toggleClass("view-less");
-          $(this).find("#view-more").toggleClass("view-less")
-        }
-      };
+        $(this).find("#view-more").toggleClass("view-less")
+      }
     })
   })
 });
