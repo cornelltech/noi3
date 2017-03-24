da2441f8d185c6a154393573abaeb3501ecdc2b7
Denny Tek <tek.denny@gmail.com>
2017-03-24 17:51:06 -0400

    Revert session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
-# Rails.application.config.session_store :cookie_store, key: '_noi3_session'
+Rails.application.config.session_store :cookie_store, key: '_noi3_session'
 
-if Rails.env.production?  
-    Rails.application.config.session_store :cookie_store, key: '_noi3_session', domain: 'networkofinnovators.org'
-else
-    Rails.application.config.session_store :cookie_store, key: '_noi3_session', domain: :all
-end
+# if Rails.env.production?  
+#     Rails.application.config.session_store :cookie_store, key: '_noi3_session', domain: 'networkofinnovators.org'
+# else
+#     Rails.application.config.session_store :cookie_store, key: '_noi3_session', domain: :all
+# end
